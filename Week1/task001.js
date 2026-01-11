2b4196d0bd0dcaeb00dbc48e22f1f81cca982be8
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-01-12 00:39:33 +0500

    Create addMultipleNumbers function

--- Week1/task001.js
+++ Week1/task001.js
@@ -4,3 +4,15 @@ function addTwoNumbers(num1, num2) {
 }
 
 console.log(addTwoNumbers(20, 90));
+
+// Add multiple numbers
+function addMultipleNumbers(...num) {
+  let sum = 0;
+  for (const n of num) {
+    sum += n;
+  }
+  return sum;
+}
+
+console.log(addMultipleNumbers(1, 2, 3, 4, 5));
+console.log(addMultipleNumbers(89, 58));
