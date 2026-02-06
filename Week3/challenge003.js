fdfc6e041170d9de886842b7588fc7ef6335897c
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-02-06 23:57:38 +0500

    feat: add a function to get rotated array k times to the right

---
+++ Week3/challenge003.js
@@ -0,0 +1,13 @@
+// 3. Rotate Array K Times to the Right
+// Description: Rotate elements of an array K times to the right.
+// Example:
+// Input: [1, 2, 3, 4, 5], K = 2
+// Output: [4, 5, 1, 2, 3]
+
+function getRightKRotated(arr, k) {
+  const rotation = k % arr.length;
+  const head = arr.slice(0, arr.length - rotation);
+  const tail = arr.slice(-rotation);
+  return [...tail, ...head];
+}
+console.log(getRightKRotated([1, 2, 3, 4, 5], 2));
