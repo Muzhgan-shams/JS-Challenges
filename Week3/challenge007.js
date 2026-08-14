ec92e1e84514296e6efa79802218a93a7fb035e9
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-08-14 23:44:33 +0430

    feat: create a function that returns factorial of a number using recursion

--- Week3/challenge007.js
+++ Week3/challenge007.js
@@ -8,3 +8,11 @@ function factorialIterative(n) {
 }
 
 console.log(factorialIterative(5)); // 120
+
+// Factorial using resursion
+function factorialRecursive(n) {
+  if (n === 0) return 1; // Base case
+  return n * factorialRecursive(n - 1); // Recursive step
+}
+
+console.log(factorialRecursive(5)); // 120
