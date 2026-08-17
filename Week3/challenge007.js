c49532d4da93586571624ec2f299a2cdb577d9ce
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-08-17 22:00:05 +0430

    feat: create isPalindrome using recursion

--- Week3/challenge007.js
+++ Week3/challenge007.js
@@ -50,3 +50,12 @@ function gcd(a, b) {
 }
 
 console.log(gcd(48, 18)); // 6
+
+// palindrome check - recursion
+function isPalindrome(str, left = 0, right = str.length - 1) {
+  if (left >= right) return true;
+  if (str[left] !== str[right]) return false;
+  return isPalindrome(str, left + 1, right - 1);
+}
+
+console.log(isPalindrome("racecar")); // true
