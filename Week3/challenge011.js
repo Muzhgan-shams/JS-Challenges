2d8888122edb5d49762a92cadd91a47706708b65
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-08-22 22:58:04 +0430

    feat: add test cases for isValid function to validate parentheses

--- Week3/challenge011.js
+++ Week3/challenge011.js
@@ -24,3 +24,9 @@ function isValid(s) {
 
   return stack.length === 0;
 }
+
+console.log(isValid("()")); // true
+console.log(isValid("()[]{}")); // true
+console.log(isValid("(]")); // false
+console.log(isValid("([)]")); // false
+console.log(isValid("{[]}")); // true
