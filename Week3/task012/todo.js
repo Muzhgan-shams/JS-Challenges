d04f983af80f3bea5677cd04efc9341d71f0afad
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-08-26 22:31:11 +0430

    feat: get HTML elements

--- Week3/task012/todo.js
+++ Week3/task012/todo.js
@@ -1,2 +1,9 @@
 let todos = [];
 let currentFilter = "all";
+
+const todoForm = document.getElementById("todoForm");
+const todoInput = document.getElementById("todoInput");
+const errorMessage = document.getElementById("errorMessage");
+const todoCount = document.getElementById("todoCount");
+const todoList = document.getElementById("todoList");
+const filterButtons = document.querySelectorAll(".filter-button");
