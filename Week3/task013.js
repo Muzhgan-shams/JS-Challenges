f2791685771065738fb27f922c06f413a009524e
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-09-01 21:43:43 +0430

    feat: implement Student class with constructor and displayInfo method in task013.js

--- Week3/task013.js
+++ Week3/task013.js
@@ -1,3 +1,18 @@
 // OOP
 // Classes(Blueprints)
 // Defines the destructure and behaveior of Objects
+
+class Student {
+  constructor(name, age) {
+    this.name = name; // instance property
+    this.age = age;
+  }
+
+  displayInfo() {
+    // method
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+  }
+}
+
+const student1 = new Student("Muzhgan", 2);
+student1.displayInfo();
