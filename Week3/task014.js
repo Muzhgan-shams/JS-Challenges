4d038f0fa8e2b4b44170d7dff13804d9574816af
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-09-19 22:13:44 +0430

    feat: implement countToThree generator function with usage examples

--- Week3/task014.js
+++ Week3/task014.js
@@ -1,2 +1,14 @@
 // Generators in JS
 // Generators in JavaScript are special functions (declared with function*) that can pause execution using yield and resume later, making them ideal for handling sequences, lazy evaluation, and complex iteration patterns.
+
+function* countToThree() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const counter = countToThree();
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
