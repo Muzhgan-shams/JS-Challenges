5bc2fd88ca5d9e158796d5cb79a8d8230fc1622d
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-09-19 22:12:50 +0430

    feat: add documentation for generators in JavaScript

---
+++ Week3/task014.js
@@ -0,0 +1,2 @@
+// Generators in JS
+// Generators in JavaScript are special functions (declared with function*) that can pause execution using yield and resume later, making them ideal for handling sequences, lazy evaluation, and complex iteration patterns.
