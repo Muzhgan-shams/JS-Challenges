57546b0060cc87d7e2736ad5209617eff4ccc4a5
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-09-21 22:16:14 +0430

    feat: add factory function example with user creation and greeting

--- Week3/task014.js
+++ Week3/task014.js
@@ -57,3 +57,16 @@ console.log(iter.next("World").value);
 // Factory Functions
 // Factory functions in JavaScript are simply functions that create and return objects
 // without using the class keyword or the new operator. They’re a flexible alternative to classes and constructors
+
+function createUser(name, age) {
+  return {
+    name,
+    age,
+    greet() {
+      console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    },
+  };
+}
+
+const user1 = createUser("Muzhgan", 5);
+user1.greet();
